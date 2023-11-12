df8c65facc19d10c40f807727fecd76174827ab9
MhmdMshhwt <85801466+MhmdMshhwt@users.noreply.github.com>
2023-11-12 11:20:14 +0200

    updated stepper font size

--- src/Pages/Registration-Flow/Stepper/stepper.js
+++ src/Pages/Registration-Flow/Stepper/stepper.js
@@ -180,6 +180,9 @@ export default function HorizontalLinearStepper() {
                     
                   },
                   '& .MuiStepLabel-label': {
+                    fontSize: {
+                      xs: '12px',
+                    },
                     '&:after': {
                       content: '""',
                       display: 'block',
